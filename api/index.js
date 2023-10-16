63ddbe291e1e255f19bfb2819c0e65aaad4e50ad
bava-bahurdeen <bavabahurdeen147@gmail.com>
2023-10-16 12:54:59 +0100

    connect database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,22 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(
+    ()=>{
+        console.log("successsfully connected mongo db");
+    }
+).catch(
+    (err)=>{
+console.error(err);
+    }
+)
+
 
 const app=express();
 
 app.listen(3000,()=>{
-
-    console.log("Server is running on port 3000");
+    console.log("Server start.......");
 })
